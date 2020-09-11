9302be324b4ef67ef08853b28ce1f91fffa078f9
jhankarpHero <61257321+jhankarpHero@users.noreply.github.com>
2020-09-11 13:06:17 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,15 +9,9 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Book from './components/Book/Book';
 import Header from './components/Header/Header';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-
-export const UserContext = createContext();
 
 function App() {
-  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
-      <p>Name: {loggedInUser.name}</p>
       <Router>
           <Header/>
           <Switch>
@@ -27,15 +21,14 @@ function App() {
             <Route path="/login">
               <Login />
             </Route>
-            <PrivateRoute path="/book/:bedType">
+            <Route path="/book/:bedType">
               <Book />
-            </PrivateRoute>
+            </Route>
             <Route exact path="/">
               <Home />
             </Route>
           </Switch>
       </Router>
-    </UserContext.Provider>
   );
 }
 
